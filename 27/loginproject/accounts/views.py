9758ea46f10752974f48c7d9a8c4d4b4340489cf
Jiyo P V <jiyopv21@gmail.com>
2026-03-01 21:37:30 +0530

    added

--- 27/loginproject/accounts/views.py
+++ 27/loginproject/accounts/views.py
@@ -1,31 +1,29 @@
-# Program 28: Protect View using login_required
+# Program 27: Login + Theme Toggle
 # @JIYO P V ROLL NO:33 Date: 01-03-2026
 
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
 
-def home(request):
-    return render(request, 'home.html')
-
-def user_login(request):
+def login_view(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
 
         user = authenticate(request, username=username, password=password)
-
-        if user is not None:
+        if user:
             login(request, user)
             return redirect('dashboard')
 
     return render(request, 'login.html')
 
-def user_logout(request):
+
+def dashboard(request):
+    if request.user.is_authenticated:
+        return render(request, 'dashboard.html')
+    else:
+        return redirect('login')
+
+
+def logout_view(request):
     logout(request)
     return redirect('login')
-
-# Protected View using decorator
-@login_required
-def dashboard(request):
-    return render(request, 'dashboard.html')
